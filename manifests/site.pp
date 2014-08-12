4ea1280305bdb2c17780caa00024b0429fb9eff0
David Orn Johannsson <davideaglephotos@gmail.com>
2014-08-12 08:16:46 -0700

    fixed include

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node 'puppet.local' {
   include ::base
   include ::puppetdb
-  include ::puppet::master::config
+  include ::puppetdb::master::config
   notify { "Generated from our notify branch": }
 }
 
