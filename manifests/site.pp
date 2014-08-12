ab3a878f3d6e5214b9d54c5ef05081c27d78fbbc
David Orn Johannsson <davideaglephotos@gmail.com>
2014-08-12 08:28:52 -0700

    added hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,14 @@
 node 'puppet.local' {
   include ::base
+
   include ::puppetdb
   include ::puppetdb::master::config
+
+  # Hiera
+  package { ['hiera', 'hiera-puppet']:
+    ensure => present,
+  }
+
   notify { "Generated from our notify branch": }
 }
 
