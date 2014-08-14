bf2be94beb3c62999c9fe3c14c49bcb3b20c71cf
David Orn Johannsson <davidoj@puppet.local>
2014-08-14 04:23:51 -0700

    updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,48 +1,9 @@
-node 'puppet.local' {
-  include ::base
-
-  include ::puppetdb
-  include ::puppetdb::master::config
-
-  # Hiera
-  package { ['hiera', 'hiera-puppet']:
-    ensure => present,
-  }
-
-  notify { "Generated from our notify branch": }
-
-  class { '::mcollective':
-    client             => true,
-    middleware         => true,
-    middleware_hosts   => [ 'puppet.local' ],
-    middleware_ssl     => true,
-    securityprovider   => 'ssl',
-    ssl_client_certs   => 'puppet:///modules/site_mcollective/client_certs',
-    ssl_ca_cert        => 'puppet:///modules/site_mcollective/certs/ca.pem',
-    ssl_server_public  => 'puppet:///modules/site_mcollective/certs/puppet.local.pem',
-    ssl_server_private => 'puppet:///modules/site_mcollective/private_keys/puppet.local.pem',
-  }
-
-  user { 'root':
-    ensure => present,
-  } ->
-  mcollective::user { 'root':
-    homedir     => '/root',
-    certificate => 'puppet:///modules/site_mcollective/client_certs/root.pem',
-    private_key => 'puppet:///modules/site_mcollective/private_keys/root.pem',
-  }
-
-  mcollective::plugin { 'puppet':
-    package => true,
-  }
-
+Package {
+  allow_virtual => true,
 }
 
-node /^server\d+/ {
-  include role::webserver
-}
-
-node default{
+node default {
+  hiera_include('classes')
 }
 
 
