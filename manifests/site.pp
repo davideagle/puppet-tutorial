16ddf1f67e3208f4e4e526e33c6e3d1544492f57
David Orn Johannsson <davidoj@puppet.local>
2014-08-15 06:40:07 -0700

    added node dhcp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,21 @@ Package {
   allow_virtual => true,
 }
 
+node /dhcp/ {
+  include ::profile::dhcp_server
+
+  ::dhcp::server::subnet { '10.0.0.0':
+    broadcast   => '10.0.0.255',
+    netmask     => '255.255.255.0',
+    routers     => '10.0.0.1',
+    range_begin => '10.0.0.100',
+    range_end   => '10.0.0.150',
+    dns_servers => ['10.0.0.1', '10.0.0.2'],
+    domain_name => 'local',
+    other_opts  => ['option ntp-servers 10.0.0.1'],
+  }
+}
+
 node default {
   hiera_include('classes')
 }
